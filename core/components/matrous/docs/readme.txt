33540a7628f6f0f71c9fe9646376ea0ecb34847f
electrica <electricaev@gmail.com>
2017-12-10 17:12:35 +0600

    Version 1.0.0-beta

--- core/components/matrous/docs/readme.txt
+++ core/components/matrous/docs/readme.txt
@@ -1,7 +1,5 @@
 --------------------
 Matrous
 --------------------
-Author: John Doe <john@doe.com>
+Author: Electrica <info@modx.kz>
 --------------------
-
-A basic Extra for MODx Revolution.
--- core/components/matrous/lexicon/ru/setting.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$_lang['area_matrous_main'] = 'Основные';
-
-$_lang['setting_matrous_some_setting'] = 'Какая-то настройка';
-$_lang['setting_matrous_some_setting_desc'] = 'Это описание для какой-то настройки';
